2160dc729886304fd77926050f758ac1c9de0062
Diego Boyatjian <dboyatjian@gmail.com>
2022-03-17 03:16:12 -0300

    carpetas git

---
+++ src/main/java/com/hotel/ereservas/EReservasApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.ereservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EReservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/ereservas/EReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.ereservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
